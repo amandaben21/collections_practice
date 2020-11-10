3f2fe0e79d5e4de2f0efa7a537d07ccceb9945db
Amanda <amandaben21@aol.com>
2020-11-09 19:27:33 -0500

    Done. The #kesha_maker and #add_s was a little difficult to do, but i got help :-)

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,45 @@
+def sort_array_asc(array)
+    array = [25,7,1]
+    array.sort do |a, b|
+        a <=> b 
+    end
+end
 
+def sort_array_desc(array)
+    array.sort! {|x, y| y <=> x}
+end
+
+def sort_array_char_count(array)
+    array.sort {|left, right| left.length <=> right.length}
+end
+
+def swap_elements(array)
+   array[0], array[1], array[2] = array[0], array[2], array[1]
+end
+
+def reverse_array(array)
+    array.reverse
+end
+
+def kesha_maker(array)
+    array.each { |i| i[2] = "$"}
+end
+
+def find_a(array)
+    array.select { |i| i.start_with? "a"}
+end
+
+def sum_array(array)
+    array.inject(0){|sum,x| sum + x}
+end
+
+def add_s(array)
+    array.collect do |element|
+        if array[1] == element
+        
+            element
+        else 
+            element + "s"
+        end
+    end
+end
